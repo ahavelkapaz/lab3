42fed06e5c546529f4d1d49f9e6b87642f0669a8
Ainhoa Havelka <Ainhoa Havelka>
2016-09-30 14:03:54 +0200

    Lab3 inicio - fallo con img en sql

--- registrar.php
+++ registrar.php
@@ -1,36 +1,40 @@
 <?php
- session_start();
- require_once 'dbconfig.php';
 
- if(isset($_POST['btn-login']))
- {
-  $user_email = trim($_POST['user_email']);
-  $user_password = trim($_POST['password']);
+ require_once 'db_config.php';
+
+
+  $user_name = $_POST['nombre'];
+  $user_email = trim($_POST['email']);
+  $user_pass = trim($_POST['password']);//SHA1
+  $user_phone = trim($_POST['telefono']);
+  $user_speciality = trim($_POST['selectE']);
+
   
-  $password = md5($user_password);
+  $user_tech= htmlspecialchars($_POST['tech']);
   
-  try
-  { 
-  
-   $stmt = $db_con->prepare("SELECT * FROM tbl_users WHERE user_email=:email");
-   $stmt->execute(array(":email"=>$user_email));
-   $row = $stmt->fetch(PDO::FETCH_ASSOC);
-   $count = $stmt->rowCount();
-   
-   if($row['user_password']==$password){
-    
-    echo "ok"; // log in
-    $_SESSION['user_session'] = $row['user_id'];
-   }
-   else{
-    
-    echo "email or password does not exist."; // wrong details 
-   }
-    
-  }
-  catch(PDOException $e){
-   echo $e->getMessage();
-  }
- }
+    //Añadir imagen
+    $image= addslashes(file_get_contents($_FILES['image']['tmp_name']));
+
+	//echo date("Y/m/d");
+	$time=date("Y-m-d H:i:s");
+	echo $time;
+ $sql="INSERT INTO users VALUES($user_name,$user_email,$user_pass,$user_phone,$user_speciality,$user_tech,'" . $image . "',$time)";
+
+ // Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+if(!mysqli_query($conn,$sql)){
+	die('Error' . mysqli_error($conn));
+}
+else{
+	echo "Mira y reza";
+}
+
+mysqli_close($conn);
+
+
 
 ?>
